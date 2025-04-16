b8d5c379fbbee5d54282047ce5d93d58664a01d3
olivcia0301 <walczynskaoliwia@gmail.com>
2025-04-16 16:33:58 +0200

    wip: Menu is added

---
+++ notatnik.py
@@ -0,0 +1,15 @@
+moja_lista_warzyw=["banan", "marchew", "jabłko"]
+for warzywo in moja_lista_warzyw:
+    print(warzywo)
+
+
+nowe_warzywo=input("podaj wazrywo")
+
+
+print(nowe_warzywo)
+
+
+moja_lista_warzyw.append(nowe_warzywo)
+
+
+print(moja_lista_warzyw)
