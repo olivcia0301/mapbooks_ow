bef67ac5e77239d4c832e17d238418df8b8b78e4
olivcia0301 <walczynskaoliwia@gmail.com>
2025-04-16 16:30:44 +0200

    wip: Menu is added

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from utils.model import users
-from utils.controller import get_user_info
-
+from utils.controller import get_user_info, add_user
 
 def main():
     print(f"Witaj {users[0]["name"]}")
-    while True:
-       print("===============MENU================")
-       print("0 - Zakończ pracę programu")
-       print("1 - Pokaż co u moich znajomych")
-       print("2 - Dodaj znajomych")
-       print("===================================")
-       choice:str=input("Wybierz opcję menu: ")
-       if choice == "0": break
-       if choice == "1": get_user_info(users[1:])
 
+    while True:
+        print("============MENU==============")
+        print("0 - Zakończ pracę programu")
+        print("1 - Pokaż co u moich znajomych")
+        print("2 - Dodaj znajomego")
+        print("==============================")
+        choice:str=input("Wybierz opcję menu: ")
+        if choice == "0": break
+        if choice == "1": get_user_info(users[1:])
+        if choice == "2": add_user(users)
 
 if __name__ == "__main__":
     main()
--- utils/model.py
+++ utils/model.py
@@ -1,7 +1,7 @@
-users:list=[
-    {"name":"Oliwia","location": "Warszawa","posts": 267},
-    {"name":"Julka","location": "Pogroszyn","posts": 767},
-    {"name":"Kamil","location": "Kraków","posts": 578},
-    {"name":"Filip","location": "Giżycko","posts": 274},
+users: list = [
+    {"name": "Mateusz", "location": "Węgorzewo", "posts": 100},
+    {"name": "Wiktoria", "location": "Chełm", "posts": 6},
+    {"name": "Sabina", "location": "Opole", "posts": 110},
+    {"name": "Weronika", "location": "Tomaszów Mazowiecki", "posts": 300},
+    {"name": "Julia", "location": "Żyrardów", "posts": 50},
 ]
-
