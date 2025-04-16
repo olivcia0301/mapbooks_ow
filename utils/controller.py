87c5adf6ce5f066819b96025e7e6bbe79c548e87
olivcia0301 <walczynskaoliwia@gmail.com>
2025-04-16 19:05:08 +0200

    wip: interactive map

--- utils/controller.py
+++ utils/controller.py
@@ -1,3 +1,9 @@
+import folium
+import requests
+from bs4 import BeautifulSoup
+
+
+
 def get_user_info(users_data: list) -> None:
     for user in users_data:
         print(f"Twój znajomy {user["name"]} z {user["location"]} opublikował {user["posts"]} postów.")
@@ -24,3 +30,30 @@ def update_user(users_data: list[dict]) -> None:
             user["name"] = input("Podaj nowe imię znajomego: ")
             user["location"] = input("Podaj nową miejscowość: ")
             user["posts"] = int(input("Podaj nową liczbę postów: "))
+
+
+
+def get_coordinates(city_name:str)->list:
+    address_url:str=f"https://pl.wikipedia.org/wiki/{city_name}"
+    response = requests.get(address_url).text
+    response_html = BeautifulSoup(response, "html.parser")
+    longitude:float=float(response_html.select(".longitude")[1].text.replace(",","."))
+    # print(longitude)
+    latitude:float=float(response_html.select(".latitude")[1].text.replace(",","."))
+    # print(latitude)
+    return [latitude, longitude]
+
+
+
+def get_map(users_data:list)->None:
+    mapa= folium.Map(location=[52.3,21.0], zoom_start=6)
+    for user in users_data:
+        folium.Marker(location=get_coordinates(city_name=user["location"]),
+                      popup=f'{user["name"]}<br/>{user["location"]}<br/> {user["posts"]}'
+                            f'<img src="{user["picture"]}" alt="{user["picture"]}"/>'
+
+        ).add_to(mapa)
+
+
+    mapa.save("mapa.html")
+
