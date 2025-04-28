eadab8b70d81d363bebcf709e768e28813b22eef
olivcia0301 <walczynskaoliwia@gmail.com>
2025-04-28 10:14:29 +0200

    chore: cleaning repository

--- utils/model.py
+++
@@ -1,7 +0,0 @@
-users: list = [
-    {"name": "Mateusz", "location": "Węgorzewo", "posts": 100, 'picture': 'https://www.wig.wat.edu.pl/images/aktualnosci/wielkanoc/wielkanoc2025.jpg'},
-    {"name": "Wiktoria", "location": "Chełm", "posts": 6, 'picture': 'https://www.wig.wat.edu.pl/images/aktualnosci/wielkanoc/wielkanoc2025.jpg'},
-    {"name": "Sabina", "location": "Opole", "posts": 110, 'picture': 'https://www.wig.wat.edu.pl/images/aktualnosci/wielkanoc/wielkanoc2025.jpg'},
-    {"name": "Weronika", "location": "Tomaszów Mazowiecki", "posts": 300, 'picture': 'https://www.wig.wat.edu.pl/images/aktualnosci/wielkanoc/wielkanoc2025.jpg'},
-    {"name": "Julia", "location": "Żyrardów", "posts": 50, 'picture': 'https://www.wig.wat.edu.pl/images/aktualnosci/wielkanoc/wielkanoc2025.jpg'},
-]
